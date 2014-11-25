c1b2be80169eb44d12d0001af73828ea5bbc7909
Ian Macalinao <ian@giza.us>
2014-11-25 03:58:20 -0600

    Fix tejas bugs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// module dependencies
+var async = require('async');
 var program = require('commander');
 var csv = require('fast-csv');
-var requireDir = require('require-dir');
-var dir = requireDir('./lib');
+var yelp = require('./lib/yelp');
 var fs = require('fs');
 
 // cli configuration
@@ -10,27 +9,38 @@ program
   .version('1.0.0')
   .command('convert <input> <output>')
   .action(function(input, output) {
-    var out = [];
-    var stream = fs.createReadStream(input);
-    var csvStream = csv()
-      .on('data', function(data) {
+
+    fs.readFile(input, function(err, data) {
+
+      var res = [];
+
+      var lines = data.toString().split('\n');
+
+      async.eachLimit(lines, 10, function(line, done) {
+
+        var data = line.split(',');
         var term = data[0].toLowerCase().split(/(?=\.[^.]+$)/)[0];
         var loc = data[1].toLowerCase() + ', ' + data[2].toLowerCase();
         var ws = fs.createWriteStream(output);
-        dir.yelp.fullLookup(term, loc, function(content, term) {
-          out.push([data[0], term]);
-          console.log(out)
+
+        yelp.fullLookup(term, loc, function(content, term) {
+          console.log(data[0] + ' ' + term);
+          res.push(data[0] + ',' + term);
+          done();
         });
-      })
-      .on('end', function() {
-        var ws = fs.createWriteStream(output);
-        csv
-          .write(out, {
-            headers: false
-          })
-          .pipe(ws);
+
+      }, function(err) {
+        if (err) { 
+          console.log(err);
+        }
+
+        fs.writeFile(output, res.join('\n'), function() {
+          console.log('Done!');
+        });
       });
-      stream.pipe(csvStream);
+
+    });
+
   });
 
 program.parse(process.argv);
