952fec98852cce99e1203b3f254251b044d4abc4
Tejas Manohar <me@tejasmanohar.com>
2014-11-26 12:54:07 -0600

    20

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ program
 
       var lines = data.toString().split('\n');
 
-      async.eachLimit(lines, 10, function(line, done) {
+      async.eachLimit(lines, 20, function(line, done) {
         var data = line.split(',');
         var loc = data[1] + ', ' + data[2];
         var ws = fs.createWriteStream(output);
